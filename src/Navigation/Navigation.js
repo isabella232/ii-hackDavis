dde00cc318a986565bfee55324c8e5a4f6016f1a
Michelle Hu <mxthu@ucdavis.edu>
2020-01-18 15:14:59 -0800

    some nav stuff

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+class Navigation extends Component {
+    render() {
+        return (
+            <nav className = "navBar">
+                <a>
+                    <img src=""/>
+                </a>
+                <Link to="/">Indigenous Interpreters</Link>
+                <ul>
+                    <li>
+                        <Link to="Search"/>
+                    </li>
+                    <li>
+                        <Link to="Resources"/>
+                    </li>
+                    <li>
+                        <Link to="Contact Us"/>
+                    </li>
+                    <button>Log In</button>
+                </ul>
+            </nav>
+        )
+    }
+}
