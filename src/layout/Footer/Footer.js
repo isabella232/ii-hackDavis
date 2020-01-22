b5d4cd8479f0597c1e842030611409960f4f123b
Bryan Zhang <36049897+poroia@users.noreply.github.com>
2020-01-22 15:26:27 -0800

    Nav Reformat
    - added footer tag for footer
    - reformat nav file hierarchy
    - created new folder layout for pages
    - restyled nav bar with 'active' effects
    
    - also sorry for the auto 2-space formatting

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -3,9 +3,9 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div className="Footer">
+    <footer className="Footer">
         <p>Indigenous Interpreters &copy;</p>
-    </div>
+    </footer>
   );
 }
 
