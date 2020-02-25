820ba353727a7db13285944665b29332fa8643a3
Long H. Nguyen <lohnguyen47@gmail.com>
2020-02-25 15:17:24 -0800

    Add view certificate modal

--- frontend/src/components/SearchPage/InterpreterInfoModal/CertificateItem/CertificateItem.js
+++ frontend/src/components/SearchPage/InterpreterInfoModal/CertificateItem/CertificateItem.js
@@ -2,12 +2,16 @@ import React from 'react';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import classes from './CertificateItem.module.css';
 
+import CertificateModal from '../../CertificateModal/CertificateModal';
+
 const CertificateItem = props => {
     return (
         <div className={classes.CertificateItem}>
-            <div className={classes.title}>{props.title}</div>
-            {props.isValidated}
-            {props.isValidated ? <CheckCircleIcon /> : null}
+            <div className={classes.header}>
+                <div className={classes.title}>{props.title}</div>
+                {props.isValidated ? <CheckCircleIcon /> : null}
+            </div>
+            <CertificateModal image={props.image} />
         </div>
     )
 }
