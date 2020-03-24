7cfb03af88cc520257d17d1801f68b60c0b2071f
Michelle Hu <mxthu@ucdavis.edu>
2020-03-24 16:49:12 -0700

    gitignore

---
+++ client/build/precache-manifest.b300a03fc1f91f32f82333565c43fb7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620fb9da13a0af5ce7cbe898cd877fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00e068330141af6ffc71",
+    "url": "/static/css/2.7c6b79a1.chunk.css"
+  },
+  {
+    "revision": "d0d3123c2943fe446a6e",
+    "url": "/static/css/main.cabf6a96.chunk.css"
+  },
+  {
+    "revision": "00e068330141af6ffc71",
+    "url": "/static/js/2.05f03359.chunk.js"
+  },
+  {
+    "revision": "0478dbbc1932f47f1614a514f6e5fb8c",
+    "url": "/static/js/2.05f03359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0d3123c2943fe446a6e",
+    "url": "/static/js/main.98c6b6f8.chunk.js"
+  },
+  {
+    "revision": "54d27813f09427fc8a0b",
+    "url": "/static/js/runtime-main.77364d2f.js"
+  }
+]);
