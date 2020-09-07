de120498b1787e6c873bb21b943f2232c95e4429
Long H. Nguyen <lohnguyen47@gmail.com>
2020-09-07 16:11:40 -0700

    Fix minor things

--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -3,7 +3,7 @@ import './css/Footer.css'
 
 const Footer = () =>
 	<footer className="Footer">
-		<p>Indigenous Interpreters &copy;</p>
+		<p>Multilingual Indigenous Interpreters &copy;</p>
 	</footer>
 
 export default Footer;
