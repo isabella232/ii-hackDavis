db1eaa932aacdc7b9b6d5932e9a3bc0fb21f51e1
Bao Le Hoai Nguyen <kevinnguyen@baos-mbp-2.home>
2020-09-10 14:55:31 -0700

    modify and add dynamic email templates

--- src/routers/client.js
+++ src/routers/client.js
@@ -1,113 +1,140 @@
-const express = require('express')
-const sharp = require('sharp')
-const Client = require('../models/client')
-const Event = require('../models/event')
-const Interpreter = require('../models/interpreter')
-const auth = require('../middleware/auth')
-const { imgUploader, getAvatarURL } = require('../utils/image')
-const { sendWelcomeEmail } = require('../utils/email')
-const { fillSignupInfo } = require('../utils/user')
+const express = require("express");
+const sharp = require("sharp");
+const Client = require("../models/client");
+const Event = require("../models/event");
+const Interpreter = require("../models/interpreter");
+const auth = require("../middleware/auth");
+const { imgUploader, getAvatarURL } = require("../utils/image");
+const { sendClientToVerifyEmail } = require("../utils/email");
+const { fillSignupInfo } = require("../utils/user");
 
-const router = new express.Router()
+const router = new express.Router();
 
 // create client account
-router.post('/api/client/create', imgUploader.single('avatar'), async (req, res) => {
+router.post(
+  "/api/client/create",
+  imgUploader.single("avatar"),
+  async (req, res) => {
     try {
-        const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-        const info = fillSignupInfo(req.body, buffer)
-        const client = new Client(info)
-        await client.save()
-        await sendWelcomeEmail(client.email, client.name, client._id.toString())
-        res.status(201).send()
+      const buffer = await sharp(req.file.buffer)
+        .resize({ width: 250, height: 250 })
+        .png()
+        .toBuffer();
+      const info = fillSignupInfo(req.body, buffer);
+      const client = new Client(info);
+      await client.save();
+      await sendClientToVerifyEmail(
+        client.email,
+        client.name,
+        client._id.toString()
+      );
+      res.status(201).send();
     } catch (e) {
-        console.log(e)
+      console.log(e);
 
-        if (e.code === 11000)
-            res.status(400).send({ message: "Email already registered. Please use another email." })
+      if (e.code === 11000)
+        res.status(400).send({
+          message: "Email already registered. Please use another email.",
+        });
 
-        res.status(400).send(e)
+      res.status(400).send(e);
     }
-}, (error, req, res, next) => {
-    console.log(error)
-    res.status(400).send({ message: error.message })
-})
+  },
+  (error, req, res, next) => {
+    console.log(error);
+    res.status(400).send({ message: error.message });
+  }
+);
 
 // get client's home page
 /*
  * reference: use async/await with map
  * https://zellwk.com/blog/async-await-in-loops/
  */
-router.get('/api/client/home', auth, async (req, res) => {
-    try {
-        const client = req.user
-        const now = new Date()
-        const events = await Event.find({ 'isArchived': false, 'forClients': true }).where('date').gte(now)
-        const promises = client.bookmarks.map(async email => {
-            const interpreter = await Interpreter.findOne({ email: email })
-            return {
-                name: interpreter.name,
-                email: interpreter.email,
-                languages: interpreter.languages,
-                location: interpreter.location.str,
-                phone: interpreter.phone,
-                rating: interpreter.rating,
-            }
-        })
-        const bookmarks = await Promise.all(promises)
-        const data = {
-            name: client.name,
-            email: client.email,
-            avatar: client.avatar.url,
-            bookmarks: bookmarks,
-            events: events
-        }
-        res.send(data)
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
-    }
-})
+router.get("/api/client/home", auth, async (req, res) => {
+  try {
+    const client = req.user;
+    const now = new Date();
+    const events = await Event.find({ isArchived: false, forClients: true })
+      .where("date")
+      .gte(now);
+    const promises = client.bookmarks.map(async (email) => {
+      const interpreter = await Interpreter.findOne({ email: email });
+      return {
+        name: interpreter.name,
+        email: interpreter.email,
+        languages: interpreter.languages,
+        location: interpreter.location.str,
+        phone: interpreter.phone,
+        rating: interpreter.rating,
+      };
+    });
+    const bookmarks = await Promise.all(promises);
+    const data = {
+      name: client.name,
+      email: client.email,
+      avatar: client.avatar.url,
+      bookmarks: bookmarks,
+      events: events,
+    };
+    res.send(data);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
 
 // update client's info
-router.patch('/api/client/updateInfo', auth, imgUploader.single('avatar'), async (req, res) => {
-    const client = req.user
+router.patch(
+  "/api/client/updateInfo",
+  auth,
+  imgUploader.single("avatar"),
+  async (req, res) => {
+    const client = req.user;
     try {
-        if (req.file) {
-            client.avatar.buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
-        }
-        client.name = req.body.name
-        await client.save()
-        res.send()
+      if (req.file) {
+        client.avatar.buffer = await sharp(req.file.buffer)
+          .resize({ width: 250, height: 250 })
+          .png()
+          .toBuffer();
+      }
+      client.name = req.body.name;
+      await client.save();
+      res.send();
     } catch (e) {
-        res.status(400).send(e)
+      res.status(400).send(e);
     }
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
-router.patch('/api/client/bookmarkInterpreter', auth, async (req, res) => {
-    const client = req.user
-    try {
-        const email = req.body.email
-        if (!client.bookmarks.includes(email)) client.bookmarks.push(email)
-        await client.save()
-        res.send()
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
-    }
-})
+router.patch("/api/client/bookmarkInterpreter", auth, async (req, res) => {
+  const client = req.user;
+  try {
+    const email = req.body.email;
+    if (!client.bookmarks.includes(email)) client.bookmarks.push(email);
+    await client.save();
+    res.send();
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
 
-router.patch('/api/client/unbookmarkInterpreter', auth, async (req, res) => {
-    const client = req.user
-    try {
-        client.bookmarks = client.bookmarks.filter(email => email !== req.body.email)
-        await client.save()
-        res.send()
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
-    }
-})
+router.patch("/api/client/unbookmarkInterpreter", auth, async (req, res) => {
+  const client = req.user;
+  try {
+    client.bookmarks = client.bookmarks.filter(
+      (email) => email !== req.body.email
+    );
+    await client.save();
+    res.send();
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
 
-module.exports = router
+module.exports = router;
